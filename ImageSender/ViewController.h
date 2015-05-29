a6be2eb87f53084e9e4379c0261d32ecc8c82125
Alex Sergienko <alexandersergienkoios@gmail.com>
2015-05-29 21:32:08 +0300

    Initial Commit

---
+++ ImageSender/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageSender
+//
+//  Created by Alexander Sergienko on 29.05.15.
+//  Copyright (c) 2015 Alexander Sergienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageSender/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageSender
+//
+//  Created by Alexander Sergienko on 29.05.15.
+//  Copyright (c) 2015 Alexander Sergienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
